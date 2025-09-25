c3aaeda74fb3dff8f226938657dffdd2c7889f42
KamiOstaku <n.milojkov.3572@vallauri.edu>
2025-09-25 11:47:54 +0200

    Update pcost

--- 4 Anno/1. Richiami di Js/07 Form e Controlli/index.js
+++ 4 Anno/1. Richiami di Js/07 Form e Controlli/index.js
@@ -1,25 +1,35 @@
 'use strict'
-let form1;
-
-window.onload = function() {
-	form1 = document.getElementById("form1")
-}
+let form1 = document.getElementById("form1")
+let txt1 = form1.querySelector("input[type=text]")
+let lst1 = form1.querySelector("select")
+let chk1 = form1.querySelectorAll("input[type=checkbox]")
 
 // richiamato dall'html
 function visualizza(index) {
 	let msg = "";
 	let items;
 	switch (index) {
+		case 1:
+			alert(txt1.value)
+			break;
+		case 2:
+			alert(lst1.value)
+			break;
+		case 3:
+			for(let i = 0; i < chk1.length;i++)
+			{
+				alert(chk1[i].value)
+			}
 
 	}
 	alert(msg);
 }
 
 
-function imposta(index){
-	let items	
-	switch(index){
+function imposta(index) {
+	let items
+	switch (index) {
 
-	}	 
+	}
 }
 
